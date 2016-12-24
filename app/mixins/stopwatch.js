e8f5b1bebb3302ab308931054add4d877d98eec7
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2016-12-24 16:58:24 +0000

    Structure improvements, adding stopwatch feature, redirecting when appropriate

---
+++ app/mixins/stopwatch.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+let timeRemaining = null;
+
+export default Ember.Mixin.create({
+
+  // https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+
+  timeRemaining,
+
+  onInitStopwatch: (function() {
+    this.initializeClock(6000);
+  }).on('init'),
+
+  getTimeRemaining: function (endTime) {
+    var t          = Date.parse(endTime) - Date.parse(new Date());
+
+    var seconds    = Math.floor((t / 1000) % 60);
+    var minutes    = Math.floor((t / 1000 / 60) % 60);
+    var hours      = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days       = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    return {
+      'total': t,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  },
+
+  initializeClock: function(endTime) {
+    var _this = this;
+
+    var timeInterval = setInterval(function() {
+      var t = _this.getTimeRemaining(endTime);
+      _this.set('timeRemaining', t);
+
+      if (t.total <= 0)
+        clearInterval(timeInterval);
+    }, 1000);
+  }
+});
