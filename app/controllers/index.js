99caaca04dee837ece895290c82f00b03af3446c
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-07-27 22:10:11 +0100

    Index: removing Stopwatch and incresing some spacing

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,3 @@
 import Ember from 'ember';
-import StopwatchMixin from '../mixins/stopwatch';
 
-let timeToStart = 10;
-
-export default Ember.Controller.extend(StopwatchMixin, {
-  timeToStart,
-
-  observesTimeRemaining: (function() {
-    if (this.timeRemaining.seconds === 0) {
-      this.redirectToGamePage();
-    }
-  }).observes('timeRemaining.seconds'),
-
-  redirectToGamePage: function() {
-    this.transitionToRoute('game', 1);
-  }
-});
+export default Ember.Controller.extend();
