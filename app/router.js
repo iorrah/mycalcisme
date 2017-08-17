94b998d29cc5cb5fe717d20c7ae8f1c876405d1a
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-08-17 23:23:04 +0100

    Router: removing code that is not in use

--- app/router.js
+++ app/router.js
@@ -10,11 +10,6 @@ Router.map(function() {
   this.route('waiting');
   this.route('tldr');
   this.route('game', { path: '/game/:journey_id' });
-  // this.route('game', { path: '/game' }, function() {
-  //   this.route('index', { path: '/' };
-  //   this.route('live-game', { path: '/live-game/:journey_id' };
-  //   this.route('game-summary', { path: '/game-summary' };
-  // });
 });
 
 export default Router;
